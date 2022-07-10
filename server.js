714389a37629c714483a6639ffb7cdf72fd87796
peterjeong1024 <jeongd1@student.douglascollege.ca>
2022-07-09 17:20:44 -0700

    2nd commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const request = require("request");
+const scheduleItem = require("node-cron");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,10 +19,14 @@ const uri = "mongodb+srv://" + username + ":" + password + "@cluster0.hdtn4.mong
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
-        console.log("MongoDB database connection established successfully");
-    })
-    // build mongodb connection
+    console.log("MongoDB database connection established successfully");
+});
+// build mongodb connection
 
+// root path init
+app.get('/', (req, res) => {
+    res.send('Server is working');
+});
 
 // define table connection API and model
 const FashionItemRouter = require('./routes/FashionItem');
@@ -40,3 +46,21 @@ app.use('/UserPlanData', UserPlanDataRouter);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+//wake up code
+const apiurl = "https://sharewardrobe-api-server.herokuapp.com/";
+var task = scheduleItem.schedule('10 * * * * *', () => {
+    console.log(apiurl + ' try');
+    request(apiurl, function(error, response, body) {
+        if (response && response.statusCode == 200) {
+            console.log(apiurl + " => " + body);
+            return;
+        }
+
+        console.error(apiurl + ' fail to connect');
+    });
+}, {
+    scheduled: false
+});
+
+task.start();
